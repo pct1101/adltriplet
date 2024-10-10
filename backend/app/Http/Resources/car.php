4181cc67cd9bad224c7b47f4c431e505f0ac6ee5
hoanganfifao4 <hoangannguyen1002002@gmail.com>
2024-10-10 12:10:08 +0700

    Bo sung file Resource

---
+++ backend/app/Http/Resources/car.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class car extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'car_id' => $this->car_id,
+            'car_name' => $this->car_name,
+            'seats' => $this->seats,
+            'model' => $this->model,
+            'license_plate' => $this->license_plate,
+            'rental_price' => $this->rental_price,
+            'car_status' => $this->car_status,
+            'mileage' => $this->mileage,
+            'car_description' => $this->car_description,
+            'brandid' => $this->brandid
+        ];
+    }
+}

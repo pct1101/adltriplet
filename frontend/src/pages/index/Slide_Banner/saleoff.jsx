b3c4651aee034d366acb891c8f5ce28ce44af39f
hothinh191 <hongocthinh1912004@gmail.com>
2024-10-12 04:27:56 +0700

    thịnh
    css trang chủ

--- frontend/src/pages/index/Slide_Banner/saleoff.jsx
+++ frontend/src/pages/index/Slide_Banner/saleoff.jsx
@@ -1,39 +1,66 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "../../../css/home.css";
+import Card from "react-bootstrap/Card";
+import { Virtual, Navigation, Pagination } from "swiper/modules";
+import { Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 function Saleoff() {
+  const [swiperRef, setSwiperRef] = useState(null);
+
+  // Create array with 500 slides
+  const [slides, setSlides] = useState(
+    Array.from({ length: 10 }).map((_, index) => `Slide ${index + 1}`)
+  );
+  const [slide, setSlide] = useState([
+    "/upload/banner 1.jpg",
+    "/upload/banner 2.jpg",
+    "/upload/banner 3.jpg",
+    "/upload/banner 4.jpg",
+    "/upload/banner 5.jpg",
+    "/upload/banner 6.jpg",
+    "/upload/banner 7.jpg",
+    "/upload/banner 8.jpg",
+    "/upload/banner 9.jpg",
+    "/upload/banner 10.jpg",
+  ]);
   return (
     <div>
-      <div className="container my-5">
-        <h2 className="text-center mb-4">CHƯƠNG TRÌNH KHUYẾN MÃI</h2>
-        <div className="row">
-          <div className="col-md-4">
-            <div className="promo-box text-center p-4 mb-4">
-              <h5>Khuyến mãi hè 2024</h5>
-              <p>Giảm giá 20% cho các dòng xe cao cấp</p>
-              <a href="#" className="btn btn-primary">
-                Xem chi tiết
-              </a>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="promo-box text-center p-4 mb-4">
-              <h5>Khuyến mãi cuối năm</h5>
-              <p>Giảm giá 15% cho các dòng xe du lịch</p>
-              <a href="#" className="btn btn-primary">
-                Xem chi tiết
-              </a>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="promo-box text-center p-4 mb-4">
-              <h5>Ưu đãi dành cho khách hàng mới</h5>
-              <p>Giảm giá 10% cho lần thuê xe đầu tiên</p>
-              <a href="#" className="btn btn-primary">
-                Xem chi tiết
-              </a>
-            </div>
-          </div>
+      <div className="container">
+        <div className="title mb-4">
+          <h1 className="text-center">CHƯƠNG TRÌNH KHUYẾN MÃI</h1>
+          <p className="text-center">
+            <span>Nhận nhiều ưu đãi khuyến mãi từ ADL TRIPLE T</span>
+          </p>
+        </div>
+        <div className="row" style={{ marginTop: "50px" }}>
+          <>
+            <Swiper
+              modules={[Virtual, Navigation, Pagination, Autoplay]}
+              onSwiper={setSwiperRef}
+              slidesPerView={3}
+              centeredSlides={false}
+              spaceBetween={30}
+              navigation={true}
+              virtual
+              autoplay={{
+                delay: 3500, // 2.5 giây mỗi lần chuyển slide
+                disableOnInteraction: false, // Không dừng autoplay khi người dùng tương tác
+              }}
+              loop={true} // Để kích hoạt loop, slider sẽ quay lại slide đầu khi đến slide cuối
+            >
+              {slide.map((slideBanner, index) => (
+                <SwiperSlide key={slideBanner} virtualIndex={index}>
+                  <div className="col">
+                    <Card.Img variant="sales" src={slideBanner} />
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </>
         </div>
       </div>
     </div>
--- frontend/src/pages/index/body/sanphamlist.js
+++ frontend/src/pages/index/body/sanphamlist.js
@@ -3,9 +3,14 @@ import "../../../css/home.css";
 
 function Sanphamlist() {
   return (
-    <div>
-      <div className="container my-5">
-        <h2 className="text-center mb-4">XE DÀNH CHO BẠN</h2>
+    <div className="bgr-products">
+      <div className="container">
+        <div className="title mb-4 mt-4">
+          <h1 className="text-center">XE DÀNH CHO BẠN</h1>
+          <p className="text-center">
+            <span>Nhận nhiều ưu đãi khuyến mãi từ ADL TRIPLE T</span>
+          </p>
+        </div>
         <div className="row">
           <div className="col-md-3">
             <div className="card mb-4">
@@ -14,148 +19,164 @@ function Sanphamlist() {
                 className="card-img-top"
                 alt="Car"
               />
+              <div className="card-tag">
+                <span className="tag-item transmission">Số tự động</span>
+                <span className="tag-item non-mortgage">Giao xe tận nơi</span>
+              </div>
               <div className="card-body">
                 <h5 className="card-title">MAYBACH-MERCEDES S 680</h5>
                 <p className="card-text">
                   5 chỗ, 5.0 ⭐, 20 chuyến, 1,126K/ngày
                 </p>
+                <hr />
                 <p className="card-text">Quận 12, Thành phố Hồ Chí Minh</p>
-                <a href="/chitietsanpham" className="btn btn-primary">
-                  Chi tiết
-                </a>
               </div>
             </div>
           </div>
           <div className="col-md-3">
             <div className="card mb-4">
               <img
-                src="https://cdn.tuoitre.vn/zoom/700_390/471584752817336320/2023/5/4/kia-seltos-2024-1-16831894071551906827168-23-0-559-1024-crop-16831895381461470581744.jpg"
+                src="https://mercedesnhatrangvn.com/wp-content/uploads/2022/07/Mercedes-Maybach-S-680-4matic.jpg"
                 className="card-img-top"
                 alt="Car"
               />
+              <div className="card-tag">
+                <span className="tag-item transmission">Số tự động</span>
+                <span className="tag-item non-mortgage">Giao xe tận nơi</span>
+              </div>
               <div className="card-body">
-                <h5 className="card-title">KIA SELTOS PREMIUM 2024</h5>
+                <h5 className="card-title">MAYBACH-MERCEDES S 680</h5>
                 <p className="card-text">
                   5 chỗ, 5.0 ⭐, 20 chuyến, 1,126K/ngày
                 </p>
+                <hr />
                 <p className="card-text">Quận 12, Thành phố Hồ Chí Minh</p>
-                <a href="#" className="btn btn-primary">
-                  Chi tiết
-                </a>
               </div>
             </div>
           </div>
           <div className="col-md-3">
             <div className="card mb-4">
               <img
-                src="https://i1-vnexpress.vnecdn.net/2022/03/23/DSCF15692JPG-1648011323.jpg?w=500&h=300&q=100&dpr=1&fit=crop&s=SvyOXjOZBdgVluHupzA30g&t=image"
+                src="https://mercedesnhatrangvn.com/wp-content/uploads/2022/07/Mercedes-Maybach-S-680-4matic.jpg"
                 className="card-img-top"
                 alt="Car"
               />
+              <div className="card-tag">
+                <span className="tag-item transmission">Số tự động</span>
+                <span className="tag-item non-mortgage">Giao xe tận nơi</span>
+              </div>
               <div className="card-body">
-                <h5 className="card-title">TOYOTA FORTURNER 2024</h5>
+                <h5 className="card-title">MAYBACH-MERCEDES S 680</h5>
                 <p className="card-text">
                   5 chỗ, 5.0 ⭐, 20 chuyến, 1,126K/ngày
                 </p>
+                <hr />
                 <p className="card-text">Quận 12, Thành phố Hồ Chí Minh</p>
-                <a href="#" className="btn btn-primary">
-                  Chi tiết
-                </a>
               </div>
             </div>
           </div>
           <div className="col-md-3">
             <div className="card mb-4">
               <img
-                src="https://cdn.tuoitre.vn/zoom/700_390/471584752817336320/2023/5/4/kia-seltos-2024-1-16831894071551906827168-23-0-559-1024-crop-16831895381461470581744.jpg"
+                src="https://mercedesnhatrangvn.com/wp-content/uploads/2022/07/Mercedes-Maybach-S-680-4matic.jpg"
                 className="card-img-top"
                 alt="Car"
               />
+              <div className="card-tag">
+                <span className="tag-item transmission">Số tự động</span>
+                <span className="tag-item non-mortgage">Giao xe tận nơi</span>
+              </div>
               <div className="card-body">
-                <h5 className="card-title">KIA SELTOS PREMIUM 2024</h5>
+                <h5 className="card-title">MAYBACH-MERCEDES S 680</h5>
                 <p className="card-text">
                   5 chỗ, 5.0 ⭐, 20 chuyến, 1,126K/ngày
                 </p>
+                <hr />
                 <p className="card-text">Quận 12, Thành phố Hồ Chí Minh</p>
-                <a href="#" className="btn btn-primary">
-                  Chi tiết
-                </a>
               </div>
             </div>
           </div>
           <div className="col-md-3">
             <div className="card mb-4">
               <img
-                src="https://cdn.tuoitre.vn/zoom/700_390/471584752817336320/2023/5/4/kia-seltos-2024-1-16831894071551906827168-23-0-559-1024-crop-16831895381461470581744.jpg"
+                src="https://mercedesnhatrangvn.com/wp-content/uploads/2022/07/Mercedes-Maybach-S-680-4matic.jpg"
                 className="card-img-top"
                 alt="Car"
               />
+              <div className="card-tag">
+                <span className="tag-item transmission">Số tự động</span>
+                <span className="tag-item non-mortgage">Giao xe tận nơi</span>
+              </div>
               <div className="card-body">
-                <h5 className="card-title">KIA SELTOS PREMIUM 2024</h5>
+                <h5 className="card-title">MAYBACH-MERCEDES S 680</h5>
                 <p className="card-text">
                   5 chỗ, 5.0 ⭐, 20 chuyến, 1,126K/ngày
                 </p>
+                <hr />
                 <p className="card-text">Quận 12, Thành phố Hồ Chí Minh</p>
-                <a href="#" className="btn btn-primary">
-                  Chi tiết
-                </a>
               </div>
             </div>
           </div>
           <div className="col-md-3">
             <div className="card mb-4">
               <img
-                src="https://cdn.tuoitre.vn/zoom/700_390/471584752817336320/2023/5/4/kia-seltos-2024-1-16831894071551906827168-23-0-559-1024-crop-16831895381461470581744.jpg"
+                src="https://mercedesnhatrangvn.com/wp-content/uploads/2022/07/Mercedes-Maybach-S-680-4matic.jpg"
                 className="card-img-top"
                 alt="Car"
               />
+              <div className="card-tag">
+                <span className="tag-item transmission">Số tự động</span>
+                <span className="tag-item non-mortgage">Giao xe tận nơi</span>
+              </div>
               <div className="card-body">
-                <h5 className="card-title">KIA SELTOS PREMIUM 2024</h5>
+                <h5 className="card-title">MAYBACH-MERCEDES S 680</h5>
                 <p className="card-text">
                   5 chỗ, 5.0 ⭐, 20 chuyến, 1,126K/ngày
                 </p>
+                <hr />
                 <p className="card-text">Quận 12, Thành phố Hồ Chí Minh</p>
-                <a href="#" className="btn btn-primary">
-                  Chi tiết
-                </a>
               </div>
             </div>
           </div>
           <div className="col-md-3">
             <div className="card mb-4">
               <img
-                src="https://cdn.tuoitre.vn/zoom/700_390/471584752817336320/2023/5/4/kia-seltos-2024-1-16831894071551906827168-23-0-559-1024-crop-16831895381461470581744.jpg"
+                src="https://mercedesnhatrangvn.com/wp-content/uploads/2022/07/Mercedes-Maybach-S-680-4matic.jpg"
                 className="card-img-top"
                 alt="Car"
               />
+              <div className="card-tag">
+                <span className="tag-item transmission">Số tự động</span>
+                <span className="tag-item non-mortgage">Giao xe tận nơi</span>
+              </div>
               <div className="card-body">
-                <h5 className="card-title">KIA SELTOS PREMIUM 2024</h5>
+                <h5 className="card-title">MAYBACH-MERCEDES S 680</h5>
                 <p className="card-text">
                   5 chỗ, 5.0 ⭐, 20 chuyến, 1,126K/ngày
                 </p>
+                <hr />
                 <p className="card-text">Quận 12, Thành phố Hồ Chí Minh</p>
-                <a href="#" className="btn btn-primary">
-                  Chi tiết
-                </a>
               </div>
             </div>
-          </div>
+          </div>{" "}
           <div className="col-md-3">
             <div className="card mb-4">
               <img
-                src="https://cdn.tuoitre.vn/zoom/700_390/471584752817336320/2023/5/4/kia-seltos-2024-1-16831894071551906827168-23-0-559-1024-crop-16831895381461470581744.jpg"
+                src="https://mercedesnhatrangvn.com/wp-content/uploads/2022/07/Mercedes-Maybach-S-680-4matic.jpg"
                 className="card-img-top"
                 alt="Car"
               />
+              <div className="card-tag">
+                <span className="tag-item transmission">Số tự động</span>
+                <span className="tag-item non-mortgage">Giao xe tận nơi</span>
+              </div>
               <div className="card-body">
-                <h5 className="card-title">KIA SELTOS PREMIUM 2024</h5>
+                <h5 className="card-title">MAYBACH-MERCEDES S 680</h5>
                 <p className="card-text">
                   5 chỗ, 5.0 ⭐, 20 chuyến, 1,126K/ngày
                 </p>
+                <hr />
                 <p className="card-text">Quận 12, Thành phố Hồ Chí Minh</p>
-                <a href="#" className="btn btn-primary">
-                  Chi tiết
-                </a>
               </div>
             </div>
           </div>

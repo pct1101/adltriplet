36d90d9d33b9986ed9443a7766431cc390ae29f7
Phạm Công Thụ <thupcps35120@fpt.edu.vn>
2024-11-02 21:27:59 +0700

    update

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            user::class,
+            car::class,
+            carimage::class,
+            feedback::class,
+            favorite::class,
         ]);
     }
 }
